e9ab4be3ea5bd13b84dc3dff2f924a4869183737
HarakeSenpai <mohamad.harakeh2001@gmail.com>
2024-03-01 02:25:18 +0200

    added the add and delete note

--- Scripts/login.js
+++ Scripts/login.js
@@ -14,3 +14,48 @@ function login() {
     alert("Invalid username or password. Please try again.");
   }
 }
+
+function addToDo() {
+  const userInput = document.getElementById("todoInput").value;
+
+  if (userInput.length > 30) {
+    alert("You have exceeded the number of letters allowed");
+    return;
+  }
+
+  const addedDiv = document.getElementById("todoList");
+  const outerWrapper = document.createElement("div");
+  outerWrapper.id = addedDiv.children.length;
+
+  const content = document.createElement("p");
+  const trashIcon = document.createElement("i");
+
+  content.innerText = userInput;
+
+  trashIcon.className = "fa-solid fa-trash";
+  trashIcon.addEventListener("click", function () {
+    deleteItem(outerWrapper.id);
+  });
+
+  outerWrapper.appendChild(content);
+  outerWrapper.appendChild(trashIcon);
+  addedDiv.appendChild(outerWrapper);
+
+  outerWrapper.style.display = "flex";
+  outerWrapper.style.maxWidth = "100%";
+
+  content.style.flex = "1";
+  content.style.float = "right";
+
+  trashIcon.style.fontSize = "20px";
+  trashIcon.style.cursor = "pointer";
+
+  document.getElementById("todoInput").value = "";
+}
+
+function deleteItem(id) {
+  const toDelete = document.getElementById(id);
+  if (toDelete) {
+    toDelete.remove();
+  }
+}
